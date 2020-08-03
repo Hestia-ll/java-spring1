033578c3f2d89452aac5917d6a11ad2192c338ae
DMJ <15206387150>
2020-08-03 15:23:42 +0800

    1

---
+++ src/main/java/cn/qphone/spring/American.java
@@ -0,0 +1,8 @@
+package cn.qphone.spring;
+
+public class American extends Person{
+    @Override
+    public void showinfo() {
+        System.out.println("i am American");
+    }
+}
---
+++ src/main/java/cn/qphone/spring/Chinese.java
@@ -0,0 +1,8 @@
+package cn.qphone.spring;
+
+public class Chinese extends Person{
+    @Override
+    public void showinfo() {
+        System.out.println("hahahaha");
+    }
+}
